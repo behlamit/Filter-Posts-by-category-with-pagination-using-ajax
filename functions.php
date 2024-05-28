e81609c0c424bee6a9d99cd7d6b24997bea1c2dd
Sunder Magar <sunder.magar@gmail.com>
2024-05-28 17:38:08 +0530

    update

--- functions.php
+++ functions.php
@@ -6,3 +6,43 @@ function custom_filter_enqueue_scripts() {
     ));
 }
 add_action('wp_enqueue_scripts', 'custom_filter_enqueue_scripts');
+
+function filter_posts_by_category() {
+    $category_ids = isset($_POST['category_ids']) ? $_POST['category_ids'] : array();
+    $paged = isset($_POST['paged']) ? $_POST['paged'] : 1;
+    
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 5,
+        'paged' => $paged,
+    );
+
+    if (!empty($category_ids)) {
+        $args['category__in'] = $category_ids;
+    }
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            echo '<div class="post-item">';
+            the_title('<h2>', '</h2>');
+            the_excerpt();
+            echo '</div>';
+        endwhile;
+
+        // Pagination
+        echo paginate_links(array(
+            'total' => $query->max_num_pages,
+            'current' => $paged,
+            'format' => '?paged=%#%',
+        ));
+    else :
+        echo 'No posts found';
+    endif;
+
+    wp_reset_postdata();
+    die();
+}
+add_action('wp_ajax_filter_posts', 'filter_posts_by_category');
+add_action('wp_ajax_nopriv_filter_posts', 'filter_posts_by_category');
